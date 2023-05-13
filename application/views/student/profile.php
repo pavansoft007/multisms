95279fd3869e4844952017ce4c785fef529b0d44
suryadk1707 <sunilnarayana.d@gmail.com>
2023-05-13 18:41:40 +0530

    attandance tab added in student profile

--- application/views/student/profile.php
+++ application/views/student/profile.php
@@ -71,6 +71,9 @@ $previous_details = json_decode($student['previous_details'], true);
 				<li>
 					<a href="#documents" data-toggle="tab"><i class="fas fa-folder-open"></i> <?php echo translate('documents'); ?></a>
 				</li>
+				<li>
+					<a href="#attandance" data-toggle="tab"><i class="far fa-chart-bar"></i> <?php echo translate('attandance'); ?></a>
+				</li>
 				<li style="float:right;margin-bottom:0px;margin-top:10px">
 							<button class="btn btn-default btn-circle" id="authentication_btn">
 								<i class="fas fa-unlock-alt"></i> <?= translate('authentication') ?>
@@ -864,6 +867,95 @@ $previous_details = json_decode($student['previous_details'], true);
 							</table>
 						</div>
 					</div>
+				</div>
+				<div id="attandance" class="tab-pane">
+				<section class="panel appear-animation mt-sm" data-appear-animation="<?=$global_config['animations'] ?>" data-appear-animation-delay="100">
+		<header class="panel-heading">
+			<h4 class="panel-title"><i class="fas fa-users"></i> <?=translate('attendance_report')?></h4>
+		</header>
+		<div class="panel-body">
+			<!-- hidden school information prints -->
+			<div class="export_title">Monthly Attendance Sheet on <?=date("F Y", strtotime('2023-05')); ?> <?php 
+				echo translate('class') .' : '. get_type_name_by_id('class', $class_id);
+				echo ' ( ' .translate('section'). ' : ' .get_type_name_by_id('section', $section_id).' )';
+				?></div>
+			
+			
+			<div class="row mt-sm">
+				<div class="col-md-offset-8 col-md-4">
+					<table class="table table-condensed table-bordered text-dark text-center">
+						<tbody>
+							<tr>
+								<td><strong>Present :</strong> <i class="far fa-check-circle hidden-print text-success"></i><span class="visible-print">P</span></td>
+								<td><strong>Absent : </strong> <i class="far fa-times-circle hidden-print text-danger"></i><span class="visible-print">A</span></td>
+								<td><strong>Holiday : </strong> <i class="fas fa-hospital-symbol hidden-print text-info"></i><span class="visible-print">H</span></td>
+								<td><strong>Late : </strong> <i class="far fa-clock hidden-print text-tertiary"></i><span class="visible-print">L</span></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="mb-lg">
+						<table class="table table-bordered table-hover table-condensed mb-none text-dark table-export">
+							<thead>
+								<tr>
+									<td><?=translate('student_name')?></td>
+<?php
+for($i = 1; $i <= $days; $i++){
+$date = $year . '-' . $month . '-' . $i;
+?>
+							<td class="text-center"><?php echo date('D', strtotime($date)); ?> <br> <?php echo date('d', strtotime($date)); ?></td>
+<?php } ?>
+									<td class="text-center text-success">Total<br>Present</td>
+									<td class="text-center text-danger">Total<br>Absent</td>
+									<td class="text-center text-tertiary">Total<br>Late</td>
+								</tr>
+							</thead>
+							<tbody>
+<?php
+foreach ($studentlist as $row):
+$total_present = 0;
+$total_absent = 0;
+$total_late = 0;
+$studentID = $row['student_id'];
+?>
+								<tr>
+									<td><?php echo $row['first_name'] . ' ' . $row['last_name']; ?></td>
+<?php
+for ($i = 1; $i <= $days; $i++) { 
+$date = date('Y-m-d', strtotime($year . '-' . $month . '-' . $i));
+$atten = $this->attendance_model->get_attendance_by_date($studentID, $date);
+?>
+							<td class="center">
+<?php if (!empty($atten)) { ?>
+								<span data-toggle="popover" data-trigger="hover" data-placement="top" data-trigger="hover" data-content="<?php echo $atten['remark']; ?>">
+<?php if ($atten['status'] == 'A') { $total_absent++; ?>
+									<i class="far fa-times-circle text-danger"></i><span class="visible-print">A</span>
+<?php } if ($atten['status'] == 'P') { $total_present++; ?>
+									<i class="far fa-check-circle text-success"></i><span class="visible-print">P</span>
+<?php } if ($atten['status'] == 'L') { $total_late++; ?>
+									<i class="far fa-clock text-info"></i><span class="visible-print">L</span>
+<?php } if ($atten['status'] == 'H'){ ?>
+									<i class="fas fa-hospital-symbol text-tertiary"></i><span class="visible-print">H</span>
+<?php } ?>
+								</span>
+<?php } ?>
+							</td>
+<?php } ?>
+									<td class="center"><?=html_escape($total_present)?></td>
+									<td class="center"><?=html_escape($total_absent)?></td>
+									<td class="center"><?=html_escape($total_late)?></td>
+									<?php endforeach; ?>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 				</div>
 			</div>
 		</div>
